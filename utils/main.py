fb5a35d55cd12500e4c7dc59e03c93a114a5de83
sergei valdemarov <valdemarovsergei@gmail.com>
2023-06-04 15:00:57 +0300

    corrected project 3

--- utils/main.py
+++ utils/main.py
@@ -1,28 +1,4 @@
-from functions import successful_operations
-from datetime import datetime
+from functions import formatted_successful_operations
 
-
-for item in successful_operations():
-    # преобразуем дату в формат DD.MM.YYYY
-    date = datetime.strptime(item["date"], "%Y-%m-%dT%H:%M:%S.%f").date()
-    date_str = date.strftime("%d.%m.%Y")
-
-    # добавляем переменную description
-    description = item['description']
-
-    # преобразуем номер карты
-    if "from" in item:
-        from_account = item.get("from")
-        account_name = from_account.split(" ")[0]
-        card_num = item["from"][item["from"].rfind(" ") + 1:]
-        card_num = card_num[:4] + " " + card_num[4:6] + "** **** " + card_num[-4:]
-        personal_account = f"{'*' * 16}{item['to'][-4:]}"
-        # выводим данные в нужном формате
-        print(f"{date_str} {description}")
-        print(f"{account_name} {card_num} -> Счет {personal_account}")
-        print(f"{item['operationAmount']['amount']} {item['operationAmount']['currency']['code']}\n")
-    else:
-        # выводим данные в нужном формате если нет from
-        print(f"{date_str} {description}")
-        print(f"Счет {personal_account}")
-        print(f"{item['operationAmount']['amount']} {item['operationAmount']['currency']['code']}\n")
+formatted_ops = formatted_successful_operations()
+print(formatted_ops)
